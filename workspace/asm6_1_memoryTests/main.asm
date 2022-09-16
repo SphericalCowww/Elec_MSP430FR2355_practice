136cd5f6d22fafe45559a7706684dc6c99206eb4
SphericalCowww <tinglin194@gmail.com>
2022-09-16 18:50:04 +0200

    - sec6.7

--- workspace/asm6_1_memoryTests/main.asm
+++ workspace/asm6_1_memoryTests/main.asm
@@ -53,20 +53,37 @@ main:										;use break point and step mode
 			mov.w	R6, Var3
 			mov.w	Const2, Var4
 
+			mov.w   #2002h, R4
+			mov.w	@R4, R6
+			mov.w   #Const1, R4
+			mov.w	@R4, R5
+
+			mov.w	#Block1, R4
+			mov.w	@R4+, R5				; + to increment by 2 because of .w
+			mov.w	@R4+, R5
+			mov.w	@R4+, R5
+			mov.b	@R4+, R5				; + to increment by 1 because of .b
+			mov.b	@R4+, R5
+
+			mov.w	#Block1, R4
+			mov.w	0(R4), 8(R4)			; copy contents at 2000h to 2008h
+			mov.w	2(R4), 10(R4)
+			mov.w	4(R4), 12(R4)
+			mov.w	6(R4), 14(R4)
 
 			jmp 	main
 
-			.data							; got to data memory
+			.data							; got to data memory at 2000h
 			.retain							; handles .data for now
 
 Const1:		.short		1234h				; constant at memory 2000h
 Const2:		.short		0CAFEh				; constant at memory 2002h
-Var1:		.space		2
+Var1:		.space		2					; .w increments by memory 2, while .b by 1
 Var2:		.space		2
 Var3:		.space		2
 Var4:		.space		2
-
-                                            
+Block1:		.short		1122h, 3344h, 5566h, 7788h, 99AAh
+Block2:		.space		8
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
