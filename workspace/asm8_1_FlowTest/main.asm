57ae13ddd2fc40fd7ce83dc5dd20c26c426df230
SphericalCowww <tinglin194@gmail.com>
2022-09-25 20:52:20 +0200

    - sec8.1

---
+++ workspace/asm8_1_FlowTest/main.asm
@@ -0,0 +1,49 @@
+;-------------------------------------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-------------------------------------------------------------------------------
+            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            
+;-------------------------------------------------------------------------------
+            .def    RESET                   ; Export program entry-point to
+                                            ; make it known to linker.
+;-------------------------------------------------------------------------------
+            .text                           ; Assemble into program memory.
+            .retain                         ; Override ELF conditional linking
+                                            ; and retain current section.
+            .retainrefs                     ; And retain any sections that have
+                                            ; references to current section.
+
+;-------------------------------------------------------------------------------
+RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+
+
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+
+main:										; the instruction addresses starts in 0x8000 in memory
+			mov.w	#0, R4					; track these addresses via PC (program counter) in registers
+			jmp		action2
+action1:
+			mov.w	#1, R4
+			jmp		done
+action2:
+			mov.w	#2, R4
+			jmp		action1
+done:
+			br		#main					; WARNING: jump to the instruction's address
+;-------------------------------------------------------------------------------
+; Stack Pointer definition
+;-------------------------------------------------------------------------------
+            .global __STACK_END
+            .sect   .stack
+            
+;-------------------------------------------------------------------------------
+; Interrupt Vectors
+;-------------------------------------------------------------------------------
+            .sect   ".reset"                ; MSP430 RESET Vector
+            .short  RESET
+            
